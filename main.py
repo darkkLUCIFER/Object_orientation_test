42e76821430053da8bb16339f5072399cfc85bfb
Mahdi <88978694+darkkLUCIFER@users.noreply.github.com>
2022-09-20 22:03:55 +0430

    last commit

--- main.py
+++ main.py
@@ -1,69 +1,43 @@
-from random import choice
+from sample import create_samples
+from advertisment import ApartmentSell, HouseSell, ApartmentRent, HouseRent, StoreSell, StoreRent
 
-from advertisment import ApartmentSell, HouseSell, ApartmentRent, HouseRent
-from user import User
-from estate import Apartment, House, Store
-from region import Region
 
-FIRST_NAME = ['ali', 'sajad', 'mahdi']
-LAST_NAME = ['shademan', 'amini', 'norouzi']
-PHONE_NUMBER = ['09151234567', '09121234567', '09351234567', '09181234567', '09211234567', '09871234567']
+class Handler:
+    ADVERTISMENT_TYPES = {
+        1: ApartmentSell, 2: ApartmentRent, 3: HouseRent, 4: HouseSell, 5: StoreSell, 6: StoreRent
+    }
+
+    SWITCHES = {
+        'r': "get_report",
+        's': 'show_all_reports',
+    }
+
+    def get_report(self):
+        for adv in self.ADVERTISMENT_TYPES.values():
+            print(adv, adv.manager.count())
+
+    def show_all(self):
+        for adv in self.ADVERTISMENT_TYPES:
+            print(adv, adv.manager.count())
+            for obj in adv.objects_list:
+                print(obj.show_detail())
+
+    def run(self):
+        print('hello world')
+        for key in self.SWITCHES:
+            print(f'{key} for {self.SWITCHES[key]}')
+        user_input = input('enter your choice: ')
+        switch = self.SWITCHES.get(user_input, None)
+        if switch is None:
+            print('invalid input')
+            self.run()
+        choice = getattr(self, switch, None)
+        choice()
+        self.run()
+
+
 if __name__ == '__main__':
-    for number in PHONE_NUMBER:
-        User(first_name=choice(FIRST_NAME), last_name=choice(LAST_NAME), phone_number=number)
+    create_samples()
+    handler = Handler()
 
-    for user in User.objects_list:
-        print(f'{user.id} \t{user.full_name}')
-
-    reg1 = Region(name='R1')
-    apt1 = Apartment(
-        user=User.objects_list[0], area=80, rooms_count=2, built_year=1393,
-        has_elevator=True, has_parking=True, floor=2, region=reg1, address='iran-mashhad'
-    )
-    # apt1.show_description()
-
-    house1 = House(
-        has_yard=True, floors_count=1, user=User.objects_list[1], area=400,
-        rooms_count=6, built_year=1370, region=reg1, address='iran-mashhad'
-    )
-    # house1.show_description()
-
-    store1 = Store(
-        user=User.objects_list[2], area=30, rooms_count=0, built_year=1390,
-        region=reg1, address='iran-mashhad'
-    )
-    # store1.show_description()
-
-    apartment_sell = ApartmentSell(
-        user=User.objects_list[0], area=80, rooms_count=2, built_year=1393,
-        has_elevator=True, has_parking=True, floor=2, region=reg1, address='iran-mashhad',
-        price_per_meter=10, discountable=True, convertable=False
-    )
-    apartment_sell.show_detail()
-
-    house_sell = HouseSell(
-        has_yard=True, floors_count=1, user=User.objects_list[1], area=400,
-        rooms_count=6, built_year=1370, region=reg1, address='iran-mashhad',
-        price_per_meter=20, discountable=False, convertable=False
-
-    )
-    house_sell.show_detail()
-
-    apartment_rent = ApartmentRent(
-        user=User.objects_list[0], area=80, rooms_count=2, built_year=1393,
-        has_elevator=True, has_parking=True, floor=2, region=reg1, address='iran-mashhad',
-        discountable=True, convertable=False, initial_price=100, monthly_price=3.5
-    )
-
-    house_rent = HouseRent(
-        has_yard=True, floors_count=1, user=User.objects_list[1], area=400,
-        rooms_count=6, built_year=1370, region=reg1, address='iran-mashhad',
-        convertable=True, initial_price=130, monthly_price=5.5
-    )
-
-    print(ApartmentSell.manager.search(region=reg1))
-    print(ApartmentRent.manager)
-    print(HouseSell.manager)
-    print(HouseRent.manager)
-    print(HouseRent.manager.get(region=reg1))
-    print(ApartmentSell.manager.search(price_per_meter__min=9))
+    handler.run()
